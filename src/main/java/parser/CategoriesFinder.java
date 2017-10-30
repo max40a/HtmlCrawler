667aaf088b29be7e4864a2415464a6064aab4e2e
Retro <max40a@mail.ru>
2017-10-30 16:17:27 +0200

    -simplify CategoriesFinder -add simply CategoriesFinderTest

--- src/main/java/parser/CategoriesFinder.java
+++ src/main/java/parser/CategoriesFinder.java
@@ -1,26 +1,22 @@
 package parser;
 
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-public class CategoriesFinder implements PropertyFinder<List<String>>{
+public class CategoriesFinder implements PropertyFinder<List<String>> {
 
     @Override
     public Optional<List<String>> findProperty(Document document) {
-        List<String> categories = new ArrayList<>();
-
-        Elements elementsByClass = document.getElementsByClass("breadcrumb");
-        Elements resultOfSelect = elementsByClass.select("span[itemprop='title']");
-        for (int i = 1; i < resultOfSelect.size(); i++) {
-            categories.add(resultOfSelect.get(i).text());
-        }
-        if(categories.isEmpty()) {
-            return Optional.empty();
-        }
-        return Optional.of(categories);
+        return Optional.of(document
+                .getElementsByClass("breadcrumb")
+                .select("span[itemprop='title']")
+                .stream()
+                .skip(1)
+                .map(Element::text)
+                .collect(Collectors.toList()));
     }
 }
