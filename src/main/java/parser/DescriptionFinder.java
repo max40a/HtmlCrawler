321aa80cf19406a081792c02fb005111cdfe6647
Retro <max40a@mail.ru>
2017-11-11 18:13:42 +0200

    -carried functional of class DescriptionFinder2 to DescriptionFinder; -remove DescriptionFinder2 -remove DescriptionFinder2Test

--- src/main/java/parser/DescriptionFinder.java
+++ src/main/java/parser/DescriptionFinder.java
@@ -8,17 +8,19 @@ import java.util.Optional;
 
 public class DescriptionFinder implements PropertyFinder<String> {
 
-    private String cssQuery = "p > strong:matchesOwn(^ПРО КНИЖКУ$)";
+    private String cssQuery1 = "p > strong:matchesOwn(^ПРО КНИЖКУ$)";
+    private String cssQuery2 = "div.tab-content > div#annotation > p";
 
     @Override
     public Optional<String> findProperty(Document document) {
         try {
-            Elements select = document.select(cssQuery);
-            if (select.isEmpty()) return Optional.empty();
-            Elements next = select.parents().next();
-            if (next.isEmpty()) return Optional.empty();
-            Element first = next.first();
-            return !first.hasText() ? Optional.empty() : Optional.ofNullable(first.text());
+            Elements select = document.select(cssQuery1);
+            if (!select.isEmpty()) {
+                return firstSearch(select);
+            } else {
+                select = document.select(cssQuery2);
+                return secondSelect(select);
+            }
         } catch (Exception e) {
             String message = String.format("%s could not find property for URL: %s, cause: %s",
                     this.getClass().getSimpleName(),
@@ -26,5 +28,17 @@ public class DescriptionFinder implements PropertyFinder<String> {
                     e.toString());
             throw new PropertyNotFoundException(message, e);
         }
+    }
+
+    private Optional<String> firstSearch(Elements currentSelect) {
+        Elements next = currentSelect.parents().next();
+        if (next.isEmpty()) return Optional.empty();
+        Element first = next.first();
+        return !first.hasText() ? Optional.empty() : Optional.ofNullable(first.text());
+    }
+
+    private Optional<String> secondSelect(Elements currentSelect) {
+        if (currentSelect.isEmpty()) return Optional.empty();
+        return Optional.ofNullable(currentSelect.text());
     }
 }
