2c0642eaa27e8d4b918117fceb66c1f0c6faa344
Retro <max40a@mail.ru>
2017-11-11 19:39:45 +0200

    -modify AuthorFinderTest and DescriptionFinderTest classes because the behavior of the methods these classes has changed;

--- src/test/java/parser/AuthorsFinderTest.java
+++ src/test/java/parser/AuthorsFinderTest.java
@@ -1,12 +1,18 @@
 package parser;
 
+import org.apache.commons.io.IOUtils;
 import org.hamcrest.collection.IsEmptyCollection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.hasItems;
@@ -17,50 +23,78 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 public class AuthorsFinderTest {
 
+    private static final String PATH_TO_TEST_DATA_V1 = "test_files/author_finder_test_file_v_1.html";
+    private static final String PATH_TO_TEST_DATA_V2 = "test_files/author_finder_test_file_v_2.html";
+
     private PropertyFinder<List<String>> testableObject;
-    private Document doc;
+    private Document docV1;
+    private Document docV2;
     private String firstTestAuthor = "FIRST TEST AUTHOR";
     private String secondTestAuthor = "SECOND TEST AUTHOR";
 
     @Before
-    public void setUp() {
-        StringBuilder testableHtml = new StringBuilder();
-        testableHtml.append("<div class=tab-pane p-x-1 collapse in active id=annotation role=tabpanel itemprop=description>");
-        testableHtml.append("<p>Орієнтовна дата надходження &mdash; 06.11.2017р.</p>");
-        testableHtml.append("<p><strong>ТЕМАТИКА</strong></p>");
-        testableHtml.append("<p>Суспільствознавство, соціальна поведінка, історія, економіка, політика, право.</p>");
-        testableHtml.append("<p><strong>ПРО АВТОРІВ</strong></p>");
-        testableHtml.append("<p><strong>");
-        testableHtml.append(firstTestAuthor);
-        testableHtml.append("</strong>");
-        testableHtml.append("some text</p>");
-        testableHtml.append("<p><strong>");
-        testableHtml.append(secondTestAuthor);
-        testableHtml.append("</strong>");
-        testableHtml.append("some text</p>");
-        testableHtml.append("<p><strong>ВІДГУКИ ПРО КНИЖКУ</strong></p>>");
-
+    public void setUp() throws IOException {
         testableObject = new AuthorsFinder();
-        doc = Jsoup.parse(testableHtml.toString(), "UTF-8");
+
+        String testableHtmlV1 = getHtml(PATH_TO_TEST_DATA_V1);
+        testableHtmlV1 = testableHtmlV1.replace("${first test author}", firstTestAuthor);
+        testableHtmlV1 = testableHtmlV1.replace("${second test author}", secondTestAuthor);
+        docV1 = Jsoup.parse(testableHtmlV1, StandardCharsets.UTF_8.name());
+
+        String testableHtmlV2 = getHtml(PATH_TO_TEST_DATA_V2);
+        testableHtmlV2 = testableHtmlV2.replace("${test author}", firstTestAuthor);
+        docV2 = Jsoup.parse(testableHtmlV2, StandardCharsets.UTF_8.name());
+    }
+
+    private String getHtml(String pathToResource) throws IOException {
+        URL resource = getClass().getClassLoader().getResource(pathToResource);
+        return IOUtils.toString(resource, StandardCharsets.UTF_8.name());
     }
 
     @Test
-    public void test_find_authors_method() {
+    public void test_find_authors_method_V1() {
         List<String> expectedList = Arrays.asList(firstTestAuthor, secondTestAuthor);
-        List<String> actualList = testableObject.findProperty(doc).get();
+        List<String> actualList = testableObject.findProperty(docV1).orElseThrow(IllegalStateException::new);
         assertThat(expectedList, is(actualList));
         assertThat(actualList, hasSize(2));
     }
 
     @Test
-    public void test_that_find_authors_method_not_return_empty_list_when_really_expected_list_with_content() {
-        List<String> actualList = testableObject.findProperty(doc).get();
+    public void test_find_authors_method_V2() {
+        List<String> expectedList = Collections.singletonList(firstTestAuthor);
+        List<String> actualList = testableObject.findProperty(docV2).orElseThrow(IllegalStateException::new);
+        assertThat(expectedList, is(actualList));
+        assertThat(actualList, hasSize(1));
+    }
+
+    @Test
+    public void test_that_find_authors_V1_method_not_return_empty_list_when_really_expected_list_with_content() {
+        List<String> actualList = testableObject.findProperty(docV1).orElseThrow(IllegalStateException::new);
         assertThat(actualList, not(IsEmptyCollection.empty()));
     }
 
     @Test
-    public void test_find_authors_method_returned_reallyExpected_content() {
-        List<String> actualList = testableObject.findProperty(doc).get();
+    public void test_that_find_authors_V2_method_not_return_empty_list_when_really_expected_list_with_content() {
+        List<String> actualList = testableObject.findProperty(docV2).orElseThrow(IllegalStateException::new);
+        assertThat(actualList, not(IsEmptyCollection.empty()));
+    }
+
+    @Test
+    public void test_find_authors_method_V1_returned_reallyExpected_content() {
+        List<String> actualList = testableObject.findProperty(docV1).orElseThrow(IllegalStateException::new);
         assertThat(actualList, hasItems(firstTestAuthor, secondTestAuthor));
+    }
+
+    @Test
+    public void test_find_authors_method_V2_returned_reallyExpected_content() {
+        List<String> actualList = testableObject.findProperty(docV2).orElseThrow(IllegalStateException::new);
+        assertThat(actualList, hasItems(firstTestAuthor));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        testableObject = null;
+        docV1 = null;
+        docV2 = null;
     }
 }
--- src/test/java/parser/DescriptionFinderTest.java
+++ src/test/java/parser/DescriptionFinderTest.java
@@ -1,35 +1,60 @@
 package parser;
 
+import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
 
 public class DescriptionFinderTest {
 
-    private String searchString = "Searched Test String";
+    private static final String PATH_TO_TEST_DATA_V1 = "test_files/description_finder_test_file_v_1.html";
+    private static final String PATH_TO_TEST_DATA_V2 = "test_files/description_finder_test_file_v_2.html";
+
     private PropertyFinder<String> testableObject;
-    private Document doc;
+    private Document docV1;
+    private Document docV2;
+    private String searchString = "SEARCHED TEST STRING";
 
     @Before
-    public void setUp() {
-        StringBuilder testableHtml = new StringBuilder();
-        testableHtml.append("<div class=tab-pane p-x-1 collapse in active id=annotation role=tabpanel itemprop=description>");
-        testableHtml.append("<p><strong>ПРО КНИЖКУ</strong></p>");
-        testableHtml.append("<p>");
-        testableHtml.append(searchString);
-        testableHtml.append("</p>");
-        testableHtml.append("<p><strong>ТЕМАТИКА</strong></p>");
-        testableHtml.append("<p>Суспільствознавство, соціальна поведінка, історія, економіка, політика, право.</p>");
-
+    public void setUp() throws IOException {
         testableObject = new DescriptionFinder();
-        doc = Jsoup.parse(testableHtml.toString(), "UTF-8");
+
+        String testableHtmlV1 = getHtml(PATH_TO_TEST_DATA_V1);
+        testableHtmlV1 = testableHtmlV1.replace("${test data}", searchString);
+        docV1 = Jsoup.parse(testableHtmlV1, StandardCharsets.UTF_8.name());
+
+        String testableHtmlV2 = getHtml(PATH_TO_TEST_DATA_V2);
+        testableHtmlV2 = testableHtmlV2.replace("${test data}", searchString);
+        docV2 = Jsoup.parse(testableHtmlV2, StandardCharsets.UTF_8.name());
+    }
+
+    private String getHtml(String pathToResource) throws IOException {
+        URL resource = getClass().getClassLoader().getResource(pathToResource);
+        return IOUtils.toString(resource, StandardCharsets.UTF_8.name());
     }
 
     @Test
-    public void test_findProperty_method() {
-        assertEquals(searchString, testableObject.findProperty(doc).get());
+    public void test_find_description_method_v1() {
+        assertEquals(searchString, testableObject.findProperty(docV1).orElseThrow(IllegalStateException::new));
+    }
+
+    @Test
+    public void test_find_description_method_v2() {
+        assertEquals(searchString, testableObject.findProperty(docV2).orElseThrow(IllegalStateException::new));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        testableObject = null;
+        docV1 = null;
+        docV2 = null;
     }
 }
