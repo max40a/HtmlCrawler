20495c46be48957c23f9beb2241a3e1b373a538f
Retro <max40a@mail.ru>
2017-12-03 19:52:14 +0200

    -add simple ElasticSearch client to Core;

--- Core/src/main/java/com/education/cherkassy/max40a/core/SimpleBookController.java
+++ Core/src/main/java/com/education/cherkassy/max40a/core/SimpleBookController.java
@@ -1,15 +1,34 @@
 package com.education.cherkassy.max40a.core;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 @RequestMapping("/api/books")
 public class SimpleBookController {
 
-    @PostMapping()
+    private final String HOST_NAME = "localhost";
+    private final int HOST_PORT = 9300;
+
+    @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public void getBook(@RequestBody String book) {
+    public void getBook(@RequestBody String book) throws UnknownHostException {
         System.out.println(book);
+        Client client = client();
+        if (client != null) {
+            System.out.println("Elastic Search Client got!");
+        }
+    }
+
+    public Client client() throws UnknownHostException {
+        return new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new TransportAddress(InetAddress.getByName(HOST_NAME), HOST_PORT));
     }
 }
